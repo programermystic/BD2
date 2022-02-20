e600e67fd9c2ae57a2567f6ecd4baad51d3efc1f
programermystic <63376541+programermystic@users.noreply.github.com>
2022-02-20 19:50:24 -0300

    Create 16.sql

---
+++ ejercicios-3/16.sql
@@ -0,0 +1,1 @@
+select country.Name as Pais,avg(city.Population) as Promedio_Poblacion from Country inner join city on Country.Code=city.CountryCode group by Country.Name order by avg(city.Population) desc;
