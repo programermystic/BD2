74dc48eca39ade3eefcf7f763a93525d41aace74
programermystic <63376541+programermystic@users.noreply.github.com>
2022-02-20 19:55:19 -0300

    Create 27.sql

---
+++ ejercicios-3/27.sql
@@ -0,0 +1,1 @@
+select actor.actor_id as ID,actor.first_name as Nombre,actor.last_name as Apellido,count(film.film_id) as Peliculas_Filmadas from actor inner join film_actor on actor.actor_id=film_actor.actor_id inner join film on film_actor.film_id=film.film_id group by actor.actor_id order by count(film.film_id) ;
