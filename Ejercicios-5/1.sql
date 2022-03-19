e01c09d15a710584020dc38cc87596a5e15985e4
programermystic <63376541+programermystic@users.noreply.github.com>
2022-03-19 12:08:05 -0300

    Create 1.sql

---
+++ Ejercicios-5/1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `it_vendas` (
+  `Id` int(11) NOT NULL,
+  `Nombre_Producto` varchar(50) NOT NULL,
+  `Precio` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Valor_Total` int(11) NOT NULL
+)
